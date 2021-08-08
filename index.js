3f47e9770ecc02a9a2b956d308555547c1e54dc5
Simon Smith <simonsmith5521@gmail.com>
2021-08-08 13:56:10 +0100

    missed removing request

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict'
 const fs = require('fs')
 const run = require('child_process').execSync
-const request = require('request')
-const rp = require('request-promise')
 const path = require('path')
 
 /**
